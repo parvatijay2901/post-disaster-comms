86e3b747dd4d2f21059aec371bd05630b7217337
Don Setiawan <landungs@uw.edu>
2024-08-15 11:24:23 -0700

    feat: Add the use of google fonts (lato) (#43)
    * feat: Add the use of google fonts (lato)
    
    * chore(deps): Update deps

--- src/support_sphere/lib/main.dart
+++ src/support_sphere/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:support_sphere/landing/landing_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,31 +13,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          dynamicSchemeVariant: DynamicSchemeVariant.fidelity,
-          seedColor: const Color.fromARGB(255, 14, 54, 70),
-          brightness: Brightness.light,
-        ),
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        useMaterial3: true,
+      title: 'Support Sphere',
+      theme: _buildTheme(
+        Brightness.light,
       ),
       home: const Landing(),
+    );
+  }
+
+  ThemeData _buildTheme(brightness) {
+    var baseTheme = ThemeData(
+      brightness: brightness,
+      colorScheme: ColorScheme.fromSeed(
+        dynamicSchemeVariant: DynamicSchemeVariant.fidelity,
+        seedColor: const Color.fromARGB(255, 14, 54, 70),
+        brightness: Brightness.light,
+      ),
+      // This is the theme of your application.
+      //
+      // TRY THIS: Try running your application with "flutter run". You'll see
+      // the application has a purple toolbar. Then, without quitting the app,
+      // try changing the seedColor in the colorScheme below to Colors.green
+      // and then invoke "hot reload" (save your changes or press the "hot
+      // reload" button in a Flutter-supported IDE, or press "r" if you used
+      // the command line to start the app).
+      //
+      // Notice that the counter didn't reset back to zero; the application
+      // state is not lost during the reload. To reset the state, use hot
+      // restart instead.
+      //
+      // This works for code too, not just values: Most code changes can be
+      // tested with just a hot reload.
+      useMaterial3: true,
+    );
+
+    return baseTheme.copyWith(
+      textTheme: GoogleFonts.latoTextTheme(baseTheme.textTheme),
     );
   }
 }
